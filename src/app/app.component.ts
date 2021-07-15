e2262721379bd1e3cda45617bc46894bc02b99aa
WalidRH <rahouwalid17@gmail.com>
2021-07-15 22:40:42 +0100

    adding proxy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -55,39 +55,13 @@ export class AppComponent {
       '2048x1209,MacIntel,Gecko,Mozilla,Netscape,Google Inc.,4g,Intel Inc.,Intel(R) UHD Graphics 630,undefined,103,4096,2418'
     );
 
-    const headers = {
-      Host: ' parcelsapp.com',
-      Connection: ' keep-alive',
-      'Content-Length': ' 304',
-      Pragma: ' no-cache',
-      'Cache-Control': ' no-cache',
-      'sec-ch-ua': ' "Opera";v="77", "Chromium";v="91", ";Not A Brand";v="99"',
-      Accept: ' application/json, text/javascript, */*; q=0.01',
-      'X-CSRF-Token':
-        ' rYE1JBdIannmMzWhx1H8b7YrIoMEkOcs17H4ot9KoXEb7NVgcy+Ny557gk7pcENaLSWMt2eUM6nEiH8bb2OAsg==',
-      'X-Requested-With': ' XMLHttpRequest',
-      'sec-ch-ua-mobile': ' ?0',
-      'User-Agent':
-        ' Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36 OPR/77.0.4054.172',
-      'Content-Type': ' application/x-www-form-urlencoded; charset=UTF-8',
-      Origin: ' https://parcelsapp.com',
-      'Sec-Fetch-Site': ' same-origin',
-      'Sec-Fetch-Mode': ' cors',
-      'Sec-Fetch-Dest': ' empty',
-      Referer: ' https://parcelsapp.com/en/tracking/',
-      'Accept-Encoding': ' gzip, deflate, br',
-      'Accept-Language': ' en-US,en;q=0.9',
-      'Access-Control-Allow-Origin': '*'
-    };
-    this.http
-      .post('https://parcelsapp.com/api/v2/parcels', bodyRequest, { headers })
-      .subscribe(
-        response => {
-          console.log('RESPONSE DATA : ', response);
-        },
-        error => {
-          console.log('ERROR DATA : ', error);
-        }
-      );
+    this.http.post('http://localhost:4200/api', bodyRequest).subscribe(
+      response => {
+        console.log('RESPONSE DATA : ', response);
+      },
+      error => {
+        console.log('ERROR DATA : ', error);
+      }
+    );
   }
 }
